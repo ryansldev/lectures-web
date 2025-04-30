49566fd8c9a81de95298bc5f98cdebaead9b40ec
ryansldev <ryanlima1309@gmail.com>
2025-04-30 12:22:00 -0300

    💄 improving textarea styles

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -5,6 +5,6 @@ interface TextareaProps extends ComponentProps<"textarea"> {}
 
 export function Textarea({ className, ...props }: TextareaProps) {
   return (
-    <textarea className={twMerge("disabled:opacity-40 bg-neutral-800 rounded-xs p-10 focus:outline-hidden focus:border-b-1 border-atom-500", className)} {...props} />
+    <textarea className={twMerge("disabled:opacity-40 bg-neutral-900 rounded-xs p-10 focus:outline-hidden focus:border-b-1 border-atom-500", className)} {...props} />
   )
 }
